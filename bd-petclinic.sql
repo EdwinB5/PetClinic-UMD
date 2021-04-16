497b1b12c0b1d99d613a77d5589431842090e1b3
Edwin Becerra <edwin.becerra@uniminuto.edu.co>
2021-04-16 16:43:07 -0500

    Create bd-petclinic.sql

---
+++ bd-petclinic.sql
@@ -0,0 +1,198 @@
+CREATE DATABASE  IF NOT EXISTS `petclinic` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE `petclinic`;
+-- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: petclinic
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.17-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `historial_visita_mascota`
+--
+
+DROP TABLE IF EXISTS `historial_visita_mascota`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `historial_visita_mascota` (
+  `id_mascota` int(11) NOT NULL,
+  `id_veterinario` int(11) NOT NULL,
+  `fecha` date DEFAULT NULL,
+  `descripcion` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id_mascota`,`id_veterinario`),
+  KEY `id_veterinario` (`id_veterinario`),
+  CONSTRAINT `historial_visita_mascota_ibfk_1` FOREIGN KEY (`id_mascota`) REFERENCES `mascota` (`id_mascota`),
+  CONSTRAINT `historial_visita_mascota_ibfk_2` FOREIGN KEY (`id_veterinario`) REFERENCES `veterinario` (`id_persona`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `historial_visita_mascota`
+--
+
+LOCK TABLES `historial_visita_mascota` WRITE;
+/*!40000 ALTER TABLE `historial_visita_mascota` DISABLE KEYS */;
+/*!40000 ALTER TABLE `historial_visita_mascota` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `mascota`
+--
+
+DROP TABLE IF EXISTS `mascota`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `mascota` (
+  `id_mascota` int(11) NOT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  `fecha_nacimiento` varchar(45) DEFAULT NULL,
+  `tipo` varchar(45) DEFAULT NULL,
+  `especie` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id_mascota`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mascota`
+--
+
+LOCK TABLES `mascota` WRITE;
+/*!40000 ALTER TABLE `mascota` DISABLE KEYS */;
+/*!40000 ALTER TABLE `mascota` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `permisos`
+--
+
+DROP TABLE IF EXISTS `permisos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `permisos` (
+  `id_persona` int(11) NOT NULL,
+  `lectura_r` tinyint(4) DEFAULT NULL,
+  `escritura_w` tinyint(4) DEFAULT NULL,
+  `ejecucion_x` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`id_persona`),
+  CONSTRAINT `FK_idPersona` FOREIGN KEY (`id_persona`) REFERENCES `persona` (`id_persona`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `permisos`
+--
+
+LOCK TABLES `permisos` WRITE;
+/*!40000 ALTER TABLE `permisos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `permisos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `persona`
+--
+
+DROP TABLE IF EXISTS `persona`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `persona` (
+  `id_persona` int(11) NOT NULL,
+  `c.c` varchar(45) DEFAULT NULL,
+  `primer_nombre` varchar(45) DEFAULT NULL,
+  `segundo_nombre` varchar(45) DEFAULT NULL,
+  `primer_apellido` varchar(45) DEFAULT NULL,
+  `segundo_apellido` varchar(45) DEFAULT NULL,
+  `direccion` varchar(45) DEFAULT NULL,
+  `telefono` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `tipo` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id_persona`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `persona`
+--
+
+LOCK TABLES `persona` WRITE;
+/*!40000 ALTER TABLE `persona` DISABLE KEYS */;
+/*!40000 ALTER TABLE `persona` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `propietario`
+--
+
+DROP TABLE IF EXISTS `propietario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `propietario` (
+  `id_persona` int(11) NOT NULL,
+  `id_mascota` int(11) NOT NULL,
+  PRIMARY KEY (`id_persona`,`id_mascota`),
+  KEY `id_mascota` (`id_mascota`),
+  CONSTRAINT `propietario_ibfk_1` FOREIGN KEY (`id_persona`) REFERENCES `persona` (`id_persona`),
+  CONSTRAINT `propietario_ibfk_2` FOREIGN KEY (`id_mascota`) REFERENCES `mascota` (`id_mascota`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `propietario`
+--
+
+LOCK TABLES `propietario` WRITE;
+/*!40000 ALTER TABLE `propietario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `propietario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `veterinario`
+--
+
+DROP TABLE IF EXISTS `veterinario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `veterinario` (
+  `id_persona` int(11) NOT NULL,
+  `especialidad` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id_persona`),
+  CONSTRAINT `veterinario_ibfk_1` FOREIGN KEY (`id_persona`) REFERENCES `persona` (`id_persona`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `veterinario`
+--
+
+LOCK TABLES `veterinario` WRITE;
+/*!40000 ALTER TABLE `veterinario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `veterinario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'petclinic'
+--
+
+--
+-- Dumping routines for database 'petclinic'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-04-16 16:40:23
